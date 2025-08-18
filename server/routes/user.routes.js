58d88b6b3e6e53aae98baeba2ee5c5a3f8ea3b69
MohammedBoukharta <mohammed.boukharta@marketingconfort.com>
2025-08-18 13:30:39 +0100

    letsee

--- server/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import protectRoute from '../middlewares/protectedRoute.js';
-import {getUserProfil} from '../controllers/user.controller.js'
-const router = express.Router();
-
-router.get("/me",protectRoute,getUserProfil);
-export default router;
